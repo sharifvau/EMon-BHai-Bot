b6b1813efb0c047e9d44209b34cbdb18c14fafc5
Djsisjj <114715211+Djsisjj@users.noreply.github.com>
2024-05-26 17:54:54 +0600

    Update teach.js

--- scripts/commands/teach.js
+++ scripts/commands/teach.js
@@ -2,41 +2,39 @@ const axios = require('axios');
 const jimp = require("jimp");
 const fs = require("fs");
 
-module.exports.config = { 
+module.exports.config = {
   name: "teach",
-  version: "0.0.2",
-  permission: 0,
+  version: "1.0.0",
+  permssion: 0,
+  credits: "EMon-BHai",
   prefix: 'awto',
-  credits: "Nayan",
-  description: "Teach sim",
-  category: "admin",
-  usages: "hi = hello",
-    cooldowns: 5,
+  description: "sim",
+  category: "user",
+    cooldowns: 2,
 };
 
   module.exports.run = async function({ api, event, args, Users, Threads, Currencies}) {
     const uid = event.senderID;
     const info = args.join(" ");
-    const apis = await axios.get('https://raw.githubusercontent.com/MOHAMMAD-NAYAN/Nayan/main/api.json')
-  const teach = apis.data.sim
+    var n = global.sim_api
     var id = Object.keys(event.mentions)[0] || event.senderID;
   var nam = await Users.getNameUser(id);
   var ThreadInfo = await api.getThreadInfo(event.threadID);
     if (!info) {
-      return api.sendMessage(`Please enter in the format:\n${global.config.PREFIX}teach hi = hello`, event.threadID);
+      return api.sendMessage(`wrong format try : teach (your ask) - (my answer):\nEx ${global.config.PREFIX}teach Hi - Hello Dear`, event.threadID);
     } else {
-      const msg = info.split("=");
+      const msg = info.split("-");
       const ask = msg[0].trim();
       const ans = msg[1].trim();
 
 
-      const img = `${teach}/sim?type=teach&ask=${ask}&ans=${ans}`
+      const img = `http://fi3.bot-hosting.net:20536/sim?type=teach&ask=${ask}&ans=${ans}`
       try {
         const response = await axios.get(img);
 
 
                 api.sendMessage({ 
-          body: `📝Your Data Added To Database Successfully\n1️⃣ASK: ${ask}\n2️⃣ANS: ${ans}`
+          body: `ASK: ${ask}\nANS: ${ans}`
                         }, event.threadID);
                       } catch (error) {
                         console.error(error);
