458f95e853668d38e113c8a49372be4683e87715
Djsisjj <114715211+Djsisjj@users.noreply.github.com>
2024-05-27 12:13:04 +0600

    Update info.js

--- scripts/commands/info.js
+++ scripts/commands/info.js
@@ -55,8 +55,8 @@ TODAY IS TIME : ${juswa}
 
 BOT IS RUNNING ${hours}:${minutes}:${seconds}.
 
-THANKS FOR USING ${global.config.BOTNAME} 『🙅🖤』`,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
+THANKS FOR USING ${global.config.BOTNAME} 『🙅🖤』`,attachment: fs.createReadStream(__dirname + "/")}, event.threadID, () => 
     fs.unlinkSync(__dirname + "/cache/1.png"));  
-      return request(encodeURI(`https://graph.facebook.com/100075290587473/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(
+      return request(encodeURI(``)).pipe(
 fs.createWriteStream(__dirname+'/cache/1.png')).on('close',() => callback());
    };
