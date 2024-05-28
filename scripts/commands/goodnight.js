b6d1bafacf6b11f07d1a32177fc9c72d99ff8b10
Djsisjj <114715211+Djsisjj@users.noreply.github.com>
2024-05-29 02:41:21 +0600

    Update goodnight.js

--- scripts/commands/goodnight.js
+++ scripts/commands/goodnight.js
@@ -14,7 +14,7 @@ module.exports.handleEvent = async function({ api, event, client, Users, __GLOBA
 	var name = await Users.getNameUser(event.senderID);
 	if (event.body.indexOf("night")==0 || event.body.indexOf("Night")==0 || event.body.indexOf("GN")==0 || event.body.indexOf("gn")==0 || event.body.indexOf("Gn")==0 || event.body.indexOf("Gd nyt")==0 || event.body.indexOf("Gd night")==0 || event.body.indexOf("Good Night")==0 || event.body.indexOf("good night")==0 || event.body.indexOf("GOOD NIGHT")==0 || event.body.indexOf("Good night")==0 || event.body.indexOf("nyt")==0 ) { 
 		var msg = {
-				body: `<3 𝙂𝙤𝙤𝙙 𝙉𝙞𝙜𝙝𝙩 𝙃𝙖𝙫𝙚 𝘼 𝙎𝙬𝙚𝙚𝙩 𝘿𝙧𝙚𝙖𝙢 😇😴 ${name} ❤️`
+				body: ` 𝙂𝙤𝙤𝙙 𝙉𝙞𝙜𝙝𝙩 𝙃𝙖𝙫𝙚 𝘼 𝙎𝙬𝙚𝙚𝙩 𝘿𝙧𝙚𝙖𝙢 😇😴 ${name} `
 			}
 			api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("❤️", event.messageID, (err) => {}, true)
