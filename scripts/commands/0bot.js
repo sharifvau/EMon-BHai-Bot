80aba43369b9c63b771c319d4904206fbad3abe1
Djsisjj <114715211+Djsisjj@users.noreply.github.com>
2024-05-29 09:36:14 +0600

    Update 0bot.js

--- scripts/commands/0bot.js
+++ scripts/commands/0bot.js
@@ -2,7 +2,7 @@ const fs = global.nodemodule["fs-extra"];
 module.exports.config = {
   name: "gobot",
   version: "1.0.1",
-  pPermssion: 0,
+  hpPermssion: 0,
   credits: "𝙈𝙧𝙏𝙤𝙢𝙓𝙭𝙓",
   description: "gibot",
   prefix:'awto',
