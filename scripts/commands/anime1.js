37018025e78889d815b0e0c49aeaff05d0b6b94a
Djsisjj <114715211+Djsisjj@users.noreply.github.com>
2024-05-29 19:11:39 +0600

    Create anime1.js

---
+++ scripts/commands/anime1.js
@@ -0,0 +1,47 @@
+module.exports.config = {
+    name: "anime1",
+    version: "1.0.0",
+    permssion: 0,
+    credits: "ZiaRein",
+    description: "anilist",
+    prefix:"true",
+    category: "random-img",
+    usages: "send message",
+    cooldowns: 5,
+    dependencies: {
+        "request": "",
+        "fs-extra": "",
+        "axios": ""
+    }
+};
+
+module.exports.run = async({ api, event, args, client, Users, Threads, __GLOBAL, Currencies }) => {
+    const axios = global.nodemodule["axios"];
+    const request = global.nodemodule["request"];
+    const fs = global.nodemodule["fs-extra"];
+    var ZiaRein = [
+"https://i.imgur.com/7Igy9Gx.png",
+"https://i.imgur.com/RzqMjeX.png",
+"https://i.imgur.com/vnFHbIM.png",
+"https://i.imgur.com/gsoou4a.png",
+"https://i.imgur.com/T1v9j7b.png",
+"https://i.imgur.com/OZRYY3g.png",
+"https://i.imgur.com/DBW1EEn.png",
+"https://i.imgur.com/ljCSZoO.png",
+"https://i.imgur.com/ulgfKma.png",
+"https://i.imgur.com/pYcfLna.png",
+"https://i.imgur.com/wn17fDi.png",
+"https://i.imgur.com/16o7E9o.png",
+"https://i.imgur.com/YGZLoC5.png",
+"https://i.imgur.com/UPxK6Dh.png",
+"https://i.imgur.com/6AoJ67h.png",
+"https://i.imgur.com/oEogoDj.png",
+"https://i.imgur.com/Kub8Cbq.png",
+"https://i.imgur.com/igDXTw8.png",
+"https://i.imgur.com/BNPkxUe.png",
+"https://i.imgur.com/q59UneJ.png",
+"https://i.imgur.com/EMvZMij.png",
+"https://i.imgur.com/1ktsYZI.png",
+"https://i.imgur.com/Lt5PDuX.png",
+"https://i.imgur.com/432WO10.png",
+      
