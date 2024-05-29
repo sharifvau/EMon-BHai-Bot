115b79129791e33698f08acd24080c4e3335f91d
Djsisjj <114715211+Djsisjj@users.noreply.github.com>
2024-05-29 19:48:57 +0600

    Create groupemoji.js

---
+++ scripts/commands/groupemoji.js
@@ -0,0 +1,18 @@
+module.exports.config = {
+	name: "groupemoji",
+	version: "1.0.0", 
+	permssion: 2,
+	credits: "HungCatMoi",
+	description: "Change your group Emoji",
+	prefix:'true',
+	category: "Box", 
+	usages: "groupemoji [name]", 
+	cooldowns: 0,
+	dependencies: [] 
+};
+
+module.exports.run = async function({ api, event, args }) {
+	var emoji = args.join(" ")
+	if (!emoji) api.sendMessage("You have not entered Emoji 💩💩", event.threadID, event.messageID)
+	else api.changeThreadEmoji(emoji, event.threadID, () => api.sendMessage(`🔨 The bot successfully changed Emoji to: ${emoji}`, event.threadID, event.messageID));
+}
