2e31d5a032ae587117e5e988b3d212210e4163af
Djsisjj <114715211+Djsisjj@users.noreply.github.com>
2024-05-29 18:49:43 +0600

    Update help.js

--- scripts/commands/help.js
+++ scripts/commands/help.js
@@ -1,207 +1,90 @@
 module.exports.config = {
-  name: "help",
-  version: "1.0.2",
-  permission: 0,
-  credits: "EMon-BHai",
-  description: "beginner's guide",
-  prefix: 'awto',
-  category: "user",
-  usages: "help [command name]",
-  cooldowns: 5,
-  envConfig: {
+	name: "help",
+	version: "1.0.2",
+	permssion: 0,
+	credits: "MrTomXxX",
+	description: "Beginner's Guide",
+	prefix:'true',
+	category: "system",
+	usages: "[Tên module]",
+	cooldowns: 5,
+	envConfig: {
 		autoUnsend: false,
-		delayUnsend: 60
+		delayUnsend: 0
 	}
 };
 
 module.exports.languages = {
-  en: {
-    moduleInfo:
-      "「 %1 」\n%2\n\n❯ usage : %3\n❯ category : %4\n❯ waiting time : %5 seconds(s)\n❯ permission : %6\n\n» module code by EMon-BHai",
-    helpList:
-      `➤ there are %1 commands & %2 categories\n\n╭ ───«~»─── ╮
-│          GUIDE         │
-╰ ───«~»─── ╯\n➤ use : "${global.config.PREFIX}${this.config.name} ‹command›" to know how to use the command\n➤ type : "${global.config.PREFIX}${this.config.name} ‹page_number›" to show the page contents\n……………………………………\n‣ bot owner : ${global.config.OPERATORNAME}\n‣ owner fbl : ${global.config.OPERATORLINK}\n……………………………………\n\n`,
-    user: "user",
-    adminGroup: "group admin",
-    adminBot: "bot admin",
-  },
+	"vi": {
+		"moduleInfo": "「 %1 」\n%2\n\n❯ Cách sử dụng: %3\n❯ Thuộc nhóm: %4\n❯ Thời gian chờ: %5 giây(s)\n❯ Quyền hạn: %6\n\n» Module code by %7 «",
+		"helpList": '[ Hiện tại đang có %1 lệnh có thể sử dụng trên bot này, Sử dụng: "%2help nameCommand" để xem chi tiết cách sử dụng! ]"',
+		"user": "Người dùng",
+        "adminGroup": "Quản trị viên nhóm",
+        "adminBot": "Quản trị viên bot"
+	},
+	"en": {
+		"moduleInfo": "「 %1 」\n%2\n\n❯ Usage: %3\n❯ Category: %4\n❯ Waiting time: %5 seconds(s)\n❯ Permission: %6\n\n» Module code by %7 «",
+		"helpList": '[ There are %1 commands on this bot, Use: "%2help nameCommand" to know how to use! ]',
+		"user": "User",
+        "adminGroup": "Admin group",
+        "adminBot": "Admin bot"
+	}
 };
-
 
 module.exports.handleEvent = function ({ api, event, getText }) {
-  const { commands } = global.client;
-  const { threadID, messageID, body } = event;  
+	const { commands } = global.client;
+	const { threadID, messageID, body } = event;
 
-  if (!body || typeof body == "undefined" || body.indexOf("help") != 0)
-    return;
-  const splitBody = body.slice(body.indexOf("help")).trim().split(/\s+/);
-  if (splitBody.length == 1 || !commands.has(splitBody[1].toLowerCase())) return;
-  const threadSetting = global.data.threadData.get(parseInt(threadID)) || {};
-  const command = commands.get(splitBody[1].toLowerCase());
-  const prefix = threadSetting.hasOwnProperty("PREFIX")
-    ? threadSetting.PREFIX
-    : global.config.PREFIX;
-  return api.sendMessage(
-    getText(
-      "moduleInfo",
-      command.config.name,
-      command.config.description,
-      `${prefix}${command.config.name} ${command.config.usages ? command.config.usages : ""
-      }`,
-      command.config.category,
-      command.config.cooldowns,
-      command.config.permission === 0
-        ? getText("user")
-        : command.config.permission === 1
-        ? getText("adminGroup")
-        : getText("adminBot"),
-      command.config.credits
-    ),
-    threadID,
-    messageID
-  );
-};
+	if (!body || typeof body == "undefined" || body.indexOf("help") != 0) return;
+	const splitBody = body.slice(body.indexOf("help")).trim().split(/\s+/);
+	if (splitBody.length == 1 || !commands.has(splitBody[1].toLowerCase())) return;
+	const threadSetting = global.data.threadData.get(parseInt(threadID)) || {};
+	const command = commands.get(splitBody[1].toLowerCase());
+	const prefix = (threadSetting.hasOwnProperty("PREFIX")) ? threadSetting.PREFIX : global.config.PREFIX;
+	return api.sendMessage(getText("moduleInfo", command.config.name, command.config.description, `${prefix}${command.config.name} ${(command.config.usages) ? command.config.usages : ""}`, command.config.commandCategory, command.config.cooldowns, ((command.config.hasPermssion == 0) ? getText("user") : (command.config.hasPermssion == 1) ? getText("adminGroup") : getText("adminBot")), command.config.credits), threadID, messageID);
+}
 
-module.exports.run = async function ({ api, event, args, getText }) {
-  const { commands } = global.client;
-  const { threadID, messageID } = event;
-  const command = commands.get((args[0] || "").toLowerCase());
-  const threadSetting = global.data.threadData.get(parseInt(threadID)) || {};
-  const { autoUnsend, delayUnsend } = global.configModule[this.config.name];
-  const prefix = threadSetting.hasOwnProperty("PREFIX")
-    ? threadSetting.PREFIX
-    : global.config.PREFIX;
+module.exports. run = function({ api, event, args, getText }) {
+	const { commands } = global.client;
+	const { threadID, messageID } = event;
+	const command = commands.get((args[0] || "").toLowerCase());
+	const threadSetting = global.data.threadData.get(parseInt(threadID)) || {};
+	const { autoUnsend, delayUnsend } = global.configModule[this.config.name];
+	const prefix = (threadSetting.hasOwnProperty("PREFIX")) ? threadSetting.PREFIX : global.config.PREFIX;
 
-  if (!command) {
-    const commandList = Array.from(commands.values());
-    const categories = new Set(commandList.map((cmd) => cmd.config.category.toLowerCase()));
-    const categoryCount = categories.size;
-
-    const categoryNames = Array.from(categories);
-    const itemsPerPage = 10;
-    const totalPages = Math.ceil(categoryNames.length / itemsPerPage);
-
-    let currentPage = 1;
-    if (args[0]) {
-      const parsedPage = parseInt(args[0]);
-      if (
-        !isNaN(parsedPage) &&
-        parsedPage >= 1 &&
-        parsedPage <= totalPages
-      ) {
-        currentPage = parsedPage;
-      } else {
-        return api.sendMessage(
-          `➤ Oops! You went too far! Please choose a page between 1 and ${totalPages}`,
-          threadID,
-          messageID
-        );
-      }
+	if (!command) {
+		const arrayInfo = [];
+		const page = parseInt(args[0]) || 1;
+    const numberOfOnePage = 8;
+    let i = 0;
+    let msg = "command list\n\n";
+    
+    for (var [name, value] of (commands)) {
+      name += 
+      arrayInfo.push(name);
     }
-    const startIdx = (currentPage - 1) * itemsPerPage;
-    const endIdx = startIdx + itemsPerPage;
-    const visibleCategories = categoryNames.slice(startIdx, endIdx);
 
-    let msg = "";
-    for (let i = 0; i < visibleCategories.length; i++) {
-      const category = visibleCategories[i];
-      const categoryCommands = commandList.filter(
-        (cmd) =>
-          cmd.config.category.toLowerCase() === category
-      );
-      const commandNames = categoryCommands.map((cmd) => cmd.config.name);
-      const numberFont = [
-        "❶",
-        "❷",
-        "❸",
-        "❹",
-        "❺",
-        "❻",
-        "❼",
-        "❽",
-        "❾",
-        "❿",
-      ];
-      msg += `[ ${numberFont[i]} ]─❍ ${
-        category.charAt(0).toLowerCase() + category.slice(1)
-      } category\n   ❯ ${commandNames.join("\n   ❯ ")}\n\n`;
-    }
-    const numberFontPage = [
-      "❶",
-      "❷",
-      "❸",
-      "❹",
-      "❺",
-      "❻",
-      "❼",
-      "❽",
-      "❾",
-      "❿",
-      "⓫",
-      "⓬",
-      "⓭",
-      "⓮",
-      "⓯",
-      "⓰",
-      "⓱",
-      "⓲",
-      "⓳",
-      "⓴",
-    ];
-    msg += `╭ ───«~»─── ╮
-│     Page ${numberFontPage[currentPage - 1]} of ${numberFontPage[totalPages - 1]}   │\n╰ ───«~»─── ╯\n➤ total pages available: ${totalPages}\n`;
-    msg += getText("helpList", commands.size, categoryCount, prefix);
+    arrayInfo.sort((a, b) => a.data - b.data);
+    
+    const startSlice = numberOfOnePage*page - numberOfOnePage;
+    i = startSlice;
+    const returnArray = arrayInfo.slice(startSlice, startSlice + numberOfOnePage);
+    
+    for (let item of returnArray) msg += `${++i}. ${global.config.PREFIX}${item}\n`; 
+    
+    const randomText = [ "Even a small amount of alcohol poured on a scorpion will drive it crazy and sting itself to death."," The crocodile can't stick its tongue out.","The oldest known animal in the world is a 405-year-old male, discovered in 2007.","Sharks, like other fish, have their reproductive organs located in the ribcage.","The eyes of the octopus have no blind spots. On average, the brain of an octopus has 300 million neurons. When under extreme stress, some octopuses even eat their trunks.","An elephant's brain weighs about 6,000g, while a cat's brain weighs only approximately 30g.","Cats and dogs have the ability to hear ultrasound.","Sheep can survive up to 2 weeks in a state of being buried in snow.","The smartest pig in the world is owned by a math teacher in Madison, Wisconsin (USA). It has the ability to memorize worksheets multiplying to 12.","Statistics show that each rattlesnake's mating lasts up to ... more than 22 hours", "Studies have found that flies are deaf.","In a lack of water, kangaroos can endure longer than camels.","","Dogs have 4 toes on their hind legs and 5 toes on each of their front paws.","The average flight speed of honey bees is 24km/h. They never sleep.","Cockroaches can live up to 9 days after having their heads cut off.","If you leave a goldfish in the dark for a long time, it will eventually turn white.","The flying record for a chicken is 13 seconds.","The mosquito that causes the most deaths to humans worldwide is the mosquito.","TThe quack of a duck doesn't resonate, and no one knows why.","Sea pond has no brain. They are also among the few animals that can turn their stomachs inside out.","Termites are active 24 hours a day and they do not sleep. Studies have also found that termites gnaw wood twice as fast when listening to heavy rock music.","Baby giraffes usually fall from a height of 1.8 meters when they are born.", "A tiger not only has a striped coat, but their skin is also streaked with stripes.."," Vultures fly without flapping their wings.","Turkeys can reproduce without mating.","Penguins are the only birds that can swim, but not fly. Nor have any penguins been found in the Arctic."," The venom of the king cobra is so toxic that just one gram can kill 150 people.","The venom of a small scorpion is much more dangerous than the venom of a large scorpion.","The length of an oyster's penis can be so 'monstrous' that it is 20 times its body size!","Rat's heart beats 650 times per minute.","The flea can jump 350 times its body length. If it also possessed that ability, a human would be able to jump the length of a football field once.","The faster the kangaroo jumps, the less energy it consumes.","Elephants are among the few mammals that can't jump! It was also discovered that elephants still stand after death.","Spiders have transparent blood."," Snails breathe with their feet.","Some lions mate more than 50 times a day.","Chuột reproduce so quickly that in just 18 months, from just 2 mice, the mother can give birth to 1 million heirs.","Hedgehog floats on water.","Alex is the world's first African gray parrot to question its own existence: What color am I?.","The reason why flamingos are pink-red in color is because they can absorb pigments from the shells of shrimp and shrimp that they eat every day."," Owls and pigeons can memorize human faces", "Cows are more dangerous than sharks","The single pair of wings on the back and the rear stabilizer help the flies to fly continuously, but their lifespan is not more than 14 days.","With a pair of endlessly long legs that can be up to 1.5 m high and weigh 20-25 kg, the ostrich can run faster than a horse. In addition, male ostriches can roar like a lion.","Kangaroos use their tails for balance, so if you lift a Kangaroo's tail off the ground, it won't be able to jump and stand.","Tigers not only have stripes on their backs but also printed on their skin. Each individual tiger is born with its own unique stripe.","If you are being attacked by a crocodile, do not try to get rid of their sharp teeth by pushing them away. Just poke the crocodile in the eye, that's their weakness.","Fleas can jump up to 200 times their height. This is equivalent to a man jumping on the Empire State Building in New York.","A cat has up to 32 muscles in the ear. That makes them have superior hearing ability","Koalas have a taste that does not change throughout life, they eat almost nothing but .. leaves of the eucalyptus tree.","The beaver's teeth do not stop growing throughout its life. If you do not want the teeth to be too long and difficult to control, the beaver must eat hard foods to wear them down.","Animals living in coastal cliffs or estuaries have extremely weird abilities. Oysters can change sex to match the mating method.","Butterflies have eyes with thousands of lenses similar to those on cameras, but they can only see red, green, and yellow..","Don't try this at home, the truth is that if a snail loses an eye, it can recover.","Giraffes do not have vocal cords like other animals of the same family, their tongues are blue-black.","Dog nose prints are like human fingerprints and can be used to identify different dogs.","alam nyo ba na ang pogi ni ken yung owner ng bot laki pa ng tite",];
+    
+    const text = `{page (${page}/${Math.ceil(arrayInfo.length/numberOfOnePage)}) help 1 help 2}\n༻𝐎𝐖𝐍𝐄𝐑:- ☞BAD BOY☜ ༺\n༒𝚈𝚘𝚞 𝙲𝚊𝚗 𝙲𝚊𝚕𝚕 𝙷𝚒𝚖 BAD BOY〠.༒\n༒𝐇𝐢𝐬 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐢𝐝༒:- ☞  ☜ \n༻𝘠𝘰𝘶𝘵𝘶𝘣𝘦 𝘭𝘪𝘯𝘬༺:- ☞  ☜\n֎𝘍𝘰𝘳 𝘈𝘯𝘺 𝘒𝘪𝘯𝘥 𝘖𝘧 𝘏𝘦𝘭𝘱 𝘫𝘰𝘪𝘯 𝘞𝘱 𝘎𝘳𝘰𝘶𝘱֍:-☞ `;  
 
-    const axios = require("axios");
-    const fs = require("fs-extra");
-    const imgP = [];
-    const img = [
-      "https://i.ibb.co/ZLnvPwQ/Picsart-23-07-24-11-03-50-602.png"
-    ];
-    const path = __dirname + "/cache/menu.png";
-    const rdimg = img[Math.floor(Math.random() * img.length)];
-
-    const { data } = await axios.get(rdimg, {
-      responseType: "arraybuffer",
-    });
-
-    fs.writeFileSync(path, Buffer.from(data, "utf-8"));
-    imgP.push(fs.createReadStream(path));
-    const msgg = {
-  body: `╭ ──────«~»────── ╮
-│   commands and categories  │
-╰ ──────«~»────── ╯\n\n`+ msg + `\n\n`
-    };
-
-    const sentMessage = await api.sendMessage(msgg, threadID, async (error, info) => {
+return api.sendMessage(msg + "\n" + text, threadID, async (error, info) => {
 			if (autoUnsend) {
-				await new Promise(resolve => setTimeout(resolve, delayUnsend * 500));
+				await new Promise(resolve => setTimeout(resolve, delayUnsend * 10000));
 				return api.unsendMessage(info.messageID);
 			} else return;
-		}, messageID);
-  } else {
-    return api.sendMessage(
-      getText(
-        "moduleInfo",
-        command.config.name,
-        command.config.description,
-        `${prefix}${command.config.name} ${command.config.usages ? command.config.usages : ""
-        }`,
-        command.config.category,
-        command.config.cooldowns,
-        command.config.permission === 0
-          ? getText("user")
-          : command.config.permission === 1
-          ? getText("adminGroup")
-          : getText("adminBot"),
-        command.config.credits
-      ),
-      threadID, async (error, info) => {
-			if (autoUnsend) {
-				await new Promise(resolve => setTimeout(resolve, delayUnsend * 500));
-				return api.unsendMessage(info.messageID);
-			} else return;
-		}, messageID);
-  }
+		});
+	}
+
+	return
+
+api.sendMessage(getText("moduleInfo", command.config.name, command.config.description, `${prefix}${command.config.name} ${(command.config.usages) ? command.config.usages : ""}`, command.config.commandCategory, command.config.cooldowns, ((command.config.hasPermssion == 0) ? getText("user") : (command.config.hasPermssion == 1) ? getText("adminGroup") : getText("adminBot")), command.config.credits), threadID, messageID);
 };
