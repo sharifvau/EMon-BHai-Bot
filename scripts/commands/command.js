e4fa37a6135b1cde77b83620b58675df24953a6e
Djsisjj <114715211+Djsisjj@users.noreply.github.com>
2024-05-29 18:44:39 +0600

    Update command.js

--- scripts/commands/command.js
+++ scripts/commands/command.js
@@ -1,12 +1,12 @@
 module.exports.config = {
-    name: "command",
+    name: "cmd",
     version: "1.0.0",
-    permission: 2,
-    credits: "ryuko",
-    description: "manage/control all bot modules",
-    prefix: true,
-    category: "operator",
-    usages: "[load/unload/loadAll/unloadAll/info] [command name]",
+    permssion: 2,
+    credits: "MrTomXxX",
+    description: "Manage/Full control of the bot's module",
+    prefix:'true',
+    category: "Admin-bot system",
+    usages: "[load/unload/loadAll/unloadAll/info] [module name]",
     cooldowns: 5,
     dependencies: {
         "fs-extra": "",
@@ -15,14 +15,13 @@ module.exports.config = {
     }
 };
 
-const loadCommand = function ({ moduleList, threadID, messageID, getText }) {
-    let operator = global.config.OPERATOR;
-            if (!operator.includes(event.senderID)) return api.sendMessage(`only bot operators can use this command.`, event.threadID, event.messageID);
+const loadCommand = function ({ moduleList, threadID, messageID }) {
+
     const { execSync } = global.nodemodule['child_process'];
     const { writeFileSync, unlinkSync, readFileSync } = global.nodemodule['fs-extra'];
     const { join } = global.nodemodule['path'];
     const { configPath, mainPath, api } = global.client;
-    const logger = require(mainPath + '/Emonc.js');
+    const logger = require(mainPath + '/utils/log');
 
     var errorList = [];
     delete require['resolve'][require['resolve'](configPath)];
@@ -49,7 +48,7 @@ const loadCommand = function ({ moduleList, threadID, messageID, getText }) {
                         if (listPackage.hasOwnProperty(packageName) || listbuiltinModules.includes(packageName)) global.nodemodule[packageName] = require(packageName);
                         else global.nodemodule[packageName] = require(moduleDir);
                     } catch {
-                        logger.loader('not found package ' + packageName + ' support for module ' + command.config.name+ 'installing.', 'warn');
+                        logger.loader('Packages not found ' + packageName + ' support for command ' + command.config.name+ 'install it...', 'warn');
                         const insPack = {};
                         insPack.stdio = 'inherit';
                         insPack.env = process.env ;
@@ -68,10 +67,10 @@ const loadCommand = function ({ moduleList, threadID, messageID, getText }) {
                             }
                             if (loadSuccess || !error) break;
                         }
-                        if (!loadSuccess || error) throw 'unable to load package ' + packageName + (' for module ') + command.config.name +', error : ' + error + ' ' + error['stack'];
+                        if (!loadSuccess || error) throw 'Unable to load package ' + packageName + (' give command ') + command.config.name +', error: ' + error + ' ' + error['stack'];
                     }
                 }
-                logger.loader('successfully downloaded the entire package for the module.' + command.config.name);
+                logger.loader(' Successfully downloaded the entire package for the command ' + command.config.name);
             }
             if (command.config.envConfig && typeof command.config.envConfig == 'Object') try {
                 for (const [key, value] of Object['entries'](command.config.envConfig)) {
@@ -85,29 +84,29 @@ const loadCommand = function ({ moduleList, threadID, messageID, getText }) {
                     if (typeof configValue[command.config.name][key] == undefined) 
                         configValue[command.config.name][key] = value || '';
                 }
-                logger.loader('loaded config' + ' ' + command.config.name);
+                logger.loader('Loaded config' + ' ' + command.config.name);
             } catch (error) {
-                throw new Error('failed to load config module, error : ' + JSON.stringify(error));
+                throw new Error('» Unable to load config module, error: ' + JSON.stringify(error));
             }
             if (command['onLoad']) try {
                 const onLoads = {};
                 onLoads['configValue'] = configValue;
                 command['onLoad'](onLoads);
             } catch (error) {
-                throw new Error('unable to onLoad module, error : ' + JSON.stringify(error), 'error');
+                throw new Error('» Unable to onLoad module, error: ' + JSON.stringify(error), 'error');
             }
             if (command.handleEvent) global.client.eventRegistered.push(command.config.name);
             (global.config.commandDisabled.includes(nameModule + '.js') || configValue.commandDisabled.includes(nameModule + '.js')) 
             && (configValue.commandDisabled.splice(configValue.commandDisabled.indexOf(nameModule + '.js'), 1),
             global.config.commandDisabled.splice(global.config.commandDisabled.indexOf(nameModule + '.js'), 1))
             global.client.commands.set(command.config.name, command)
-            logger.loader('loaded command ' + command.config.name + '.');
+            logger.loader('Loaded command ' + command.config.name + '!');
         } catch (error) {
-            errorList.push('' + nameModule + ' reason : ' + error + ' at ' + error['stack']);
+            errorList.push('- ' + nameModule + ' reason:' + error + ' at ' + error['stack']);
         };
     }
-    if (errorList.length != 0) api.sendMessage('modules that had problems loading : ' + errorList.join(' '), threadID, messageID);
-    api.sendMessage('loaded ' + (moduleList.length - errorList.length) + ' module.', threadID, messageID) 
+    if (errorList.length != 0) api.sendMessage('» Commands that went wrong while loading: ' + errorList.join(' '), threadID, messageID);
+    api.sendMessage('» Downloaded successfully ' + (moduleList.length - errorList.length) + ' command(s)', threadID, messageID) 
     writeFileSync(configPath, JSON.stringify(configValue, null, 4), 'utf8')
     unlinkSync(configPath + '.temp');
     return;
@@ -116,7 +115,7 @@ const loadCommand = function ({ moduleList, threadID, messageID, getText }) {
 const unloadModule = function ({ moduleList, threadID, messageID }) {
     const { writeFileSync, unlinkSync } = global.nodemodule["fs-extra"];
     const { configPath, mainPath, api } = global.client;
-    const logger = require(mainPath + "/Emonc.js").loader;
+    const logger = require(mainPath + "/utils/log").loader;
 
     delete require.cache[require.resolve(configPath)];
     var configValue = require(configPath);
@@ -127,29 +126,37 @@ const unloadModule = function ({ moduleList, threadID, messageID }) {
         global.client.eventRegistered = global.client.eventRegistered.filter(item => item !== nameModule);
         configValue["commandDisabled"].push(`${nameModule}.js`);
         global.config["commandDisabled"].push(`${nameModule}.js`);
-        logger(`unloaded command ${nameModule}.`);
+        logger(`Unloaded command ${nameModule}!`);
     }
 
     writeFileSync(configPath, JSON.stringify(configValue, null, 4), 'utf8');
     unlinkSync(configPath + ".temp");
 
-    return api.sendMessage(`unloaded ${moduleList.length} module`, threadID, messageID);
+    return api.sendMessage(`» Canceled successfully ${moduleList.length} command(s)`, threadID, messageID);
 }
 
 module.exports.run = function ({ event, args, api }) {
+    
     const { readdirSync } = global.nodemodule["fs-extra"];
     const { threadID, messageID } = event;
-
+  
+const permission = global.config.GOD;
+  	if (!permission.includes(event.senderID)) return api.sendMessage(`⚠️You don't have permission to use this command!`, event.threadID, event.messageID);
     var moduleList = args.splice(1, args.length);
-
     switch (args[0]) {
+      case "count": {
+      let commands = client.commands.values();
+		  let infoCommand = "";
+			api.sendMessage("» Currently available " + client.commands.size + " command can use!"+ infoCommand, event.threadID, event.messageID);
+      break;
+		}
         case "load": {
-            if (moduleList.length == 0) return api.sendMessage("module name cannot be empty.", threadID, messageID);
+            if (moduleList.length == 0) return api.sendMessage("» Module name cannot be empty!", threadID, messageID);
             else return loadCommand({ moduleList, threadID, messageID });
         }
         case "unload": {
-            if (moduleList.length == 0) return api.sendMessage("module name cannot be empty.", threadID, messageID);
-            else return unloadModule({ moduleList, threadID, messageID });
+            if (moduleList.length == 0) return api.sendMessage("» Module name cannot be empty!", threadID, messageID);
+            else return unloadModule({ moduleList, threadID,   messageID });
         }
         case "loadAll": {
             moduleList = readdirSync(__dirname).filter((file) => file.endsWith(".js") && !file.includes('example'));
@@ -164,22 +171,15 @@ module.exports.run = function ({ event, args, api }) {
         case "info": {
             const command = global.client.commands.get(moduleList.join("") || "");
 
-            if (!command) return api.sendMessage("the module you entered does not exist.", threadID, messageID);
+            if (!command) return api.sendMessage("» The module you entered does not exist!", threadID, messageID);
 
             const { name, version, hasPermssion, credits, cooldowns, dependencies } = command.config;
 
             return api.sendMessage(
-                "" + name.toUpperCase() + "\n" +
-                 "coded by : " + credits + "\n" +
-                 "version : " + version + "\n" +
-                 "request permission : " + ((hasPermssion == 0) ? "user" : (hasPermssion == 1) ? "admin" : "bot operator" ) + "\n" +
-                 "timeout : " + cooldowns + " seconds\n" +
-                 `required packages : ${(Object.keys(dependencies || {})).join(", ") || "none"}`,
-                threadID, messageID
-            );
-        }
-        default: {
-            return global.utils.throwError(this.config.name, threadID, messageID);
-        }
-    }
-}
+                "=== " + name.toUpperCase() + " ===\n" +
+                "- Coded by: " + credits + "\n" +
+                "- Version: " + version + "\n" +
+                "- Permission Request: " + ((hasPermssion == 0) ? "User" : (hasPermssion == 1) ? "Administrators" : "bot operator" ) + "\n" +
+                "- Waiting time: " + cooldowns + " second(s)\n" +
+                `- Packages required: ${(Object.keys(dependencies || {})).join(", ") || "Not available"}`,
+                                    
