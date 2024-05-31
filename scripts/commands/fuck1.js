b66d4a04e7ac281397d8af5723280e964db984ff
Djsisjj <114715211+Djsisjj@users.noreply.github.com>
2024-06-01 03:16:21 +0600

    Create fuck1.js

---
+++ scripts/commands/fuck1.js
@@ -0,0 +1,35 @@
+module.exports.config = {
+    name: "fuck",
+    version: "3.1.1",
+    permssion: 2,
+    credits: "MrTomXxX",
+    description: "Get fuck",
+    prefix:"true",
+  category: "nsfw",
+    usages: "[@mention]",
+    cooldowns: 5,
+    dependencies: {
+        "axios": "",
+        "fs-extra": "",
+        "path": "",
+        "jimp": ""
+    }
+};
+
+module.exports.onLoad = async() => {
+    const { resolve } = global.nodemodule["path"];
+    const { existsSync, mkdirSync } = global.nodemodule["fs-extra"];
+    const { downloadFile } = global.utils;
+    const dirMaterial = __dirname + `/cache/canvas/`;
+    const path = resolve(__dirname, 'cache/canvas', 'fuckv3.png');
+    if (!existsSync(dirMaterial + "canvas")) mkdirSync(dirMaterial, { recursive: true });
+    if (!existsSync(path)) await downloadFile("https://i.ibb.co/TW9Kbwr/images-2022-08-14-T183542-356.jpg", path);
+}
+
+async function makeImage({ one, two }) {
+    const fs = global.nodemodule["fs-extra"];
+    const path = global.nodemodule["path"];
+    const axios = global.nodemodule["axios"]; 
+    const jimp = global.nodemodule["jimp"];
+    const __root = path.resolve(__dirname, "cache", "canvas");
+
