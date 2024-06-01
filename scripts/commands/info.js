c2a95220e8a92d7cde150e2fbb57bc4a7e986b91
Djsisjj <114715211+Djsisjj@users.noreply.github.com>
2024-06-01 19:49:12 +0600

    Update info.js

--- scripts/commands/info.js
+++ scripts/commands/info.js
@@ -1,14 +1,12 @@
 module.exports.config = {
-    name: "info",
-    version: "1.0.0",
-    permission: 0,
-    credits: "Emon",
-    prefix: 'awto',
-    description: "Admin Information",
-    category: "admin",
-    usages: "info",
-    cooldowns: 5,
-    dependencies: 
+	name: "inf",
+	version: "1.0.1", 
+	permssion: 0,
+	credits: "Joshua Sy", //don't change the credits please
+	description: "Admin and Bot info.",
+	category: "...",
+	cooldowns: 1,
+	dependencies: 
 	{
     "request":"",
     "fs-extra":"",
@@ -24,34 +22,30 @@ const time = process.uptime(),
 		minutes = Math.floor((time % (60 * 60)) / 60),
 		seconds = Math.floor(time % 60);
 const moment = require("moment-timezone");
-var juswa = moment.tz("Asia/Dhaka").format("『D/MM/YYYY』 【hh:mm:ss】");
-  
-var callback = () => api.sendMessage({body:` আসালামু আলাইকুম ❤️জ্বি বলুন আমি সুনছি❤️ 
+var juswa = moment.tz("Asia/Dhaka").format("『D/MM/YYYY』 【HH:mm:ss】");
+var link = ["https://i.postimg.cc/SxMdTCmz/20230531-224238.jpg","https://i.postimg.cc/TYGWMbjD/20230531-224325.jpg", "https://i.postimg.cc/BvDqnY4Q/20230531-224339.jpg",];
+var callback = () => api.sendMessage({body:`➢🄾🅆🄽🄴🅁   🄰🄽🄳   🄱🄾🅃  🄸🄽🄵🄾
 
---------------------------------------------
+^𝗕𝗢𝗧 𝗡𝗔𝗠𝗘: ${global.config.BOTNAME}
 
-FACEBOOK :  LOADING 
+^𝗕𝗢𝗧 𝗣𝗥𝗘𝗙𝗜𝗫: ${global.config.PREFIX}
 
-NAME :  ROCK EXE
+^𝘽𝙊𝙏 𝙊𝙉𝙒𝙀𝙍 :𝗕𝗔𝗗𝗕𝗢𝗬,
 
-GENDER : MALE
+^𝗙𝗔𝗖𝗘𝗕𝗢𝗢𝗞 𝗟𝗜𝗡𝗞: 𝘼𝘿𝘿 𝙆𝙊𝙍𝘼𝙍 𝙏𝙄𝙈𝗘 𝙉𝙔𝙔😺
 
-Age : 00.1
+➳✴️𝙈𝘼𝙎𝙏𝙀𝙍 𝙊𝙁 𝘽𝙊𝙏'𝙎 𝙄𝙉𝙎𝙄𝘿𝙀✴️
 
-Relationship : SINGLE 😓
+^𝗕𝗢𝗧 𝗔𝗗𝗠𝗜𝗡 : 𝘽𝘼𝘿𝘽𝙊𝙔 
 
-Work : Student 
+^𝗨𝗣𝗧𝗜𝗠𝗘 𝗩𝗘𝗥𝗦𝗜𝗢𝗡 ✨: 30.0.1
 
-FACEBOOK LINK : ADD KORI NAI🐣
+^𝗧𝗢𝗗𝗔𝗬 𝗜𝗦📜: ${juswa} 
 
+^𝗕𝗢𝗧 𝗜𝗦 𝗥𝗨𝗡𝗡𝗜𝗡𝗚⌚ ${hours}:${minutes}:${seconds}.
 
-TELEGRAM : আ্ঁম্মু্ঁ এ্ঁগু্ঁলা্ঁ চা্ঁলা্ঁতে্ঁ মা্ঁনা্ঁ ক্ঁরে্ঁছে্ঁ🐰💦
+^𝐁𝐎𝐓 𝐔𝐍𝐃𝐄𝐑 𝐏𝐑𝐎𝐓𝐄𝐂𝐓𝐄𝐃 𝐁𝐘 𝐀𝐃𝐌𝐈𝐍𝐒 😘
 
-Mail : আ্ঁব্বু্ঁ ব্ঁলে্ঁছে্ঁ জি্ঁমে্ঁল্ঁ দি্ঁলে্ঁ বি্ঁয়া্ঁ ক্ঁরা্ঁবে্ঁ না্ঁহ্ঁ🐰💦
-
-➟ UPTIME
-
-TODAY IS TIME : ${juswa} 
-
-BOT IS RUNNING ${hours}:${minutes}:${seconds}.
-
+^𝙏𝙝𝙖𝙣𝙠𝙨 𝙁𝙤𝙧 𝙐𝙨𝙞𝙣𝙜 ${global.config.BOTNAME} Bot!`,attachment: fs.createReadStream(__dirname + "/cache/juswa.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/juswa.jpg")); 
+      return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/juswa.jpg")).on("close",() => callback());
+   };
