9d7e5b6c8d7568c34f6c8c9e7cf5958e6cc10563
EMON-BHAI <sharifvau4@gmail.com>
2024-06-18 08:05:51 +0800

    Update removeBG.js

--- scripts/commands/removeBG.js
+++ scripts/commands/removeBG.js
@@ -1,62 +1,51 @@
 module.exports.config = {
-  name: 'rbg',
-  version: '1.1.1',
-  permssion: 0,
-  credits: 'EMon-BHai',
-  prefix: 'awto',
-  description: 'Remove Photo Background',
-  category: 'user',
-  usages: 'rbg ( Reply images or url images )',
-  cooldowns: 2,
-  dependencies: {
-       'form-data': '',
-       'image-downloader': ''
+    name: "rbg",
+    version: "1.0.0",
+    permission: 0,
+    credits: "Nayan",
+    description: "",
+    prefix: 'awto',
+    category: "user",
+    usages: "reply",
+    cooldowns: 10,
+    dependencies: {
+       'nayan-server': ''
     }
 };
 
-const axios = require('axios');
-const FormData = require('form-data');
-const fs = require('fs-extra');
-const path = require('path');
-const {image} = require('image-downloader');
-module.exports.run = async function({
-    api, event, args
-}){
-    try {
-        if (event.type !== "message_reply") return api.sendMessage("You must reply to a photo", event.threadID, event.messageID);
-        if (!event.messageReply.attachments || event.messageReply.attachments.length == 0) return api.sendMessage("You must reply to a photo", event.threadID, event.messageID);
-        if (event.messageReply.attachments[0].type != "photo") return api.sendMessage("This is not an image", event.threadID, event.messageID);
 
-        const content = (event.type == "message_reply") ? event.messageReply.attachments[0].url : args.join(" ");
-        const KeyApi = ["yxBXi5jTsbax38KauCMFU4bJ","Rh8giMHqNDFwx3CZRW6iJcoF","ke4vcFaSkRAFju2aB9uzmSge","YFobnEtrch553rL5ydKvLeMS","gsHprNw2huCMjc1Vje1nFhTy","rp7if4rdrzkwtvF1ygyJXkJg","Xj9grNLoTFmULT1jRjU62EWb"]
-        const inputPath = path.resolve(__dirname, 'cache', `photo.png`);
-         await image({
-        url: content, dest: inputPath
-    });
-        const formData = new FormData();
-        formData.append('size', 'auto');
-        formData.append('image_file', fs.createReadStream(inputPath), path.basename(inputPath));
-        axios({
-            method: 'post',
-            url: 'https://api.remove.bg/v1.0/removebg',
-            data: formData,
-            responseType: 'arraybuffer',
-            headers: {
-                ...formData.getHeaders(),
-                'X-Api-Key': KeyApi[Math.floor(Math.random() * KeyApi.length)],
-            },
-            encoding: null
-        })
-            .then((response) => {
-                if (response.status != 200) return console.error('Error:', response.status, response.statusText);
-                fs.writeFileSync(inputPath, response.data);
-                return api.sendMessage({ attachment: fs.createReadStream(inputPath) }, event.threadID, () => fs.unlinkSync(inputPath));
-            })
-            .catch((error) => {
-                return console.error('Request failed:', error);
-            });
-     } catch (e) {
-        console.log(e)
-        return api.sendMessage(`có cái nịt`, event.threadID, event.messageID);
-  }
-};
+
+
+
+module.exports.run = async function({ api, event, args }) {
+    const axios = require("axios")
+    const request = require("request")
+    const fs = require("fs-extra")
+    const {removebg} = require('nayan-server')
+          if (event.type !== "message_reply") return api.sendMessage("[⚜️]➜ You must reply to a photo", event.threadID, event.messageID);
+        if (!event.messageReply.attachments || event.messageReply.attachments.length == 0) return api.sendMessage("[⚜️]➜ You must reply to a photo", event.threadID, event.messageID);
+        if (event.messageReply.attachments[0].type != "photo") return api.sendMessage("[⚜️]➜ This is not an image", event.threadID, event.messageID);
+  const content = (event.type == "message_reply") ? event.messageReply.attachments[0].url : args.join(" ");
+
+const res = await removebg(content)
+  console.log(res)
+  const img1 = res.data
+        var msg = [];
+        
+  let imgs1 = (await axios.get(`${img1}`, {
+    responseType: 'arraybuffer'
+  })).data;
+  fs.writeFileSync(__dirname + "/cache/removebg.jpg", Buffer.from(imgs1, "utf-8"));
+  var allimage = [];
+  allimage.push(fs.createReadStream(__dirname + "/cache/removebg.jpg"));
+  
+        {
+            msg += `🖼️=== [ REMOVING BACKGROUND ] ===🖼️`
+        }
+
+        return api.sendMessage({
+            body: msg,
+            attachment: allimage 
+
+        }, event.threadID, event.messageID);
+}
