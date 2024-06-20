77f6b2fe8f646fc455409c6212d8cc69d237aaba
EMON-BHAI <sharifvau4@gmail.com>
2024-06-20 23:15:57 +0800

    Update imgur.js

--- scripts/commands/imgur.js
+++ scripts/commands/imgur.js
@@ -1,40 +1,41 @@
-module.exports.config = {
-  name: "imgur",
-  version: "1.0.0",
-  permission: 0,
-  credits: "Nayan",
-  description: "",
-  prefix: true, 
-  category: "user", 
-  usages: "Link",
-  cooldowns: 5,
-  dependencies: {
-    "imgur-upload-api": ''
+const axios = require("axios");
+class Imgur {
+  constructor() {
+    this.clientId = "fc9369e9aea767c", this.client = axios.create({
+      baseURL: "https://api.imgur.com/3/",
+      headers: {
+        Authorization: `Client-ID ${this.clientId}`
+      }
+    })
   }
-};
-
-module.exports.run = async function({ api, event, args }) {
-  const linkanh = event.messageReply.attachments[0].url || args.join(" ");
-    const axios = require("axios")
-    const request = require("request")
-    const fs = require("fs-extra")
-  var imgur = require('imgur-upload-api'),
-  path = require('path');
-  const myClientID = 'Client-ID 3fb071726880bbb'
-  imgur.setClientID(myClientID);
-
-  imgur.upload(linkanh, function (err,res) {
-    console.log(res)
-    const link = res.data.link;
-    const type = res.data.type;
-    var msg = [];
-    {
-        msg += `TYPE: ${type}\nLINK: ${link}`
-    }
-    return api.sendMessage({
-        body: msg
-
-    }, event.threadID, event.messageID);
-  });
-
+  async uploadImage(url) {
+    return (await this.client.post("image", {
+      image: url
+    })).data.data.link
+  }
 }
+class Modules extends Imgur {
+  constructor() {
+    super()
+  }
+  get config() {
+    return {
+      name: "imgur",
+      description: "Upload image to imgur",
+      version: "1.0.0",
+      credits: "Emon",
+      prefix: 'awto',
+      cooldown: 5,
+      usage: "imgur <url>",
+      category: "Tools ✅",
+      permssion: 0
+    }
+  }
+  run = async ({ api, event }) => {
+    var array = [];
+    if ("message_reply" != event.type || event.messageReply.attachments.length < 0) return api.sendMessage("🌸একটি ফটো রিপ্লাই করুন✅", event.threadID, event.messageID);
+    for (let { url } of event.messageReply.attachments) await this.uploadImage(url).then((res => array.push(res))).catch((err => console.log(err)));
+    return api.sendMessage(`» 👉 ${array.length} টি ছবি আপলোড করা হয়েছে৷✅\nব্যর্থতা :👉 ${array.length - event.messageReply.attachments.length}❌\n» ছবির লিঙ্ক:👉\n${array.join("\n")}✅`, event.threadID, event.messageID)
+  }
+}
+module.exports = new Modules;
